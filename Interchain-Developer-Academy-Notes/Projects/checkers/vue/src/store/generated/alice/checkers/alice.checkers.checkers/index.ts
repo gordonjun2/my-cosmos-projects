4b9401fcf6cbf9b392b3cc2d153a7676721341e1
gordonjun <gordonoh@yahoo.com.sg>
2023-06-06 10:47:40 +0800

    continue with Interchain Developer Academy

--- Interchain-Developer-Academy-Notes/Projects/checkers/vue/src/store/generated/alice/checkers/alice.checkers.checkers/index.ts
+++ Interchain-Developer-Academy-Notes/Projects/checkers/vue/src/store/generated/alice/checkers/alice.checkers.checkers/index.ts
@@ -230,18 +230,18 @@ export default {
 		},
 		
 		
-		async sendMsgCreateGame({ rootGetters }, { value, fee = [], memo = '' }) {
+		async sendMsgPlayMove({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
-				const msg = await txClient.msgCreateGame(value)
+				const msg = await txClient.msgPlayMove(value)
 				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
 	gas: "200000" }, memo})
 				return result
 			} catch (e) {
 				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgCreateGame:Init Could not initialize signing client. Wallet is required.')
+					throw new Error('TxClient:MsgPlayMove:Init Could not initialize signing client. Wallet is required.')
 				}else{
-					throw new Error('TxClient:MsgCreateGame:Send Could not broadcast Tx: '+ e.message)
+					throw new Error('TxClient:MsgPlayMove:Send Could not broadcast Tx: '+ e.message)
 				}
 			}
 		},
@@ -260,17 +260,32 @@ export default {
 				}
 			}
 		},
-		
-		async MsgCreateGame({ rootGetters }, { value }) {
+		async sendMsgCreateGame({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
 				const msg = await txClient.msgCreateGame(value)
-				return msg
+				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
+	gas: "200000" }, memo})
+				return result
 			} catch (e) {
 				if (e == MissingWalletError) {
 					throw new Error('TxClient:MsgCreateGame:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgCreateGame:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
+		
+		async MsgPlayMove({ rootGetters }, { value }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgPlayMove(value)
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgPlayMove:Init Could not initialize signing client. Wallet is required.')
 				} else{
-					throw new Error('TxClient:MsgCreateGame:Create Could not create message: ' + e.message)
+					throw new Error('TxClient:MsgPlayMove:Create Could not create message: ' + e.message)
 				}
 			}
 		},
@@ -284,6 +299,19 @@ export default {
 					throw new Error('TxClient:MsgCreatePost:Init Could not initialize signing client. Wallet is required.')
 				} else{
 					throw new Error('TxClient:MsgCreatePost:Create Could not create message: ' + e.message)
+				}
+			}
+		},
+		async MsgCreateGame({ rootGetters }, { value }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgCreateGame(value)
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgCreateGame:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgCreateGame:Create Could not create message: ' + e.message)
 				}
 			}
 		},
